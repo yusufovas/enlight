ebc9493335ecc4592d43ce95a0a9448b20b49042
yusufovas <94529654+yusufovas@users.noreply.github.com>
2022-09-14 10:54:41 +0500

    .

--- src/model/model.sql
+++ src/model/model.sql
@@ -40,14 +40,3 @@ create table tasks(
     status boolean not null default false,
     group_id int references groups(group_id) on delete cascade
 );
-
-
-insert into courses (title, price) values ('English', 550000), ('Math', 500000), ('Italian', 340000), ('React', 600000);
-
-insert into users (user_name, user_password, user_status) values ('Mike', 'admin', 1), ('Alice', 'teacher', 2), ('Eliza', 'teacher', 2), ('Alex', 'student', 3), ('Matteo', 'student', 3), ('Lex', 'student', 3), ('John', 'student', 3), ('Joe', 'student', 3), ('David', 'student', 3), ('Betty', 'student', 3)
-
-insert into groups (course_id, group_title, teacher_id) values (1, 'N1 English', 2), (1, 'N2 English', 2), (2, 'N1 MATH', 3), (3, 'Italian N1', 3), (4, 'React Development', 3);
-
-insert into group_members (student_id, group_id) values (3, 1), (5, 1), (4, 1), (7, 1), (9, 3), (10, 3), (6, 4), (3, 4), (5, 4), (7, 5), (9, 5), (10, 5)
-
-insert into tasks(task_title, group_id) values ('n1 english task-1', 1), ('n1 english task-2', 1);
